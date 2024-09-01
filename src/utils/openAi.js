5053658a35df5e6ccd445574e40cc64bf749e313
Anees Khan <aneesleaders@gmail.com>
2024-09-01 17:04:45 +0530

    .

--- src/utils/openAi.js
+++ src/utils/openAi.js
@@ -1,9 +1,9 @@
-import OpenAI from "openai";
+// import OpenAI from "openai";
 
-const client = new OpenAI({
-    apiKey: process.env.REACT_APP_API_KEY,
-    dangerouslyAllowBrowser: true
-  });
+// const client = new OpenAI({
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     dangerouslyAllowBrowser: true
+//   });
 
 
-export default client;
+// export default client;
