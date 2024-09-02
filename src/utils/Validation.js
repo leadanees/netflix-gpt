5ec53bef9ecdebaf12e9f62bf6efea468c07b632
Anees Khan <aneesleaders@gmail.com>
2024-09-02 23:02:42 +0530

    anees

--- src/utils/Validation.js
+++
@@ -1,8 +0,0 @@
-export const CheckedData = (email,password) => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
-    if(!emailRegex) return "Email ID is not Valid";
-    if(!passwordRegex) return "Password is not valid"
-
-    return null;
-}
--- src/utils/openAi.js
+++
@@ -1,9 +0,0 @@
-// import OpenAI from "openai";
-
-// const client = new OpenAI({
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     dangerouslyAllowBrowser: true
-//   });
-
-
-// export default client;
